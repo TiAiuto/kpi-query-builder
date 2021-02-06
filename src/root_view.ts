0e842275b7fe9cf8e19b33770618ba12b25fde37
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-06 22:04:50 +0900

    :heavy_plus_sign: rootViewの定義追加

--- src/root_view.ts
+++ src/root_view.ts
@@ -1,7 +1,26 @@
 import { View } from "./view";
 
 export class RootView extends View {
-  constructor() {
-    super({ type: "root" });
+  physicalSource: string;
+  physicalSourceAlias: string;
+  dateSuffixEnabled: boolean;
+
+  constructor({
+    name,
+    alphabetName,
+    physicalSource,
+    physicalSourceAlias,
+    dateSuffixEnabled,
+  }: {
+    name: string;
+    alphabetName: string;
+    physicalSource: string;
+    physicalSourceAlias: string;
+    dateSuffixEnabled: boolean;
+  }) {
+    super({ type: "root", name, alphabetName });
+    this.physicalSource = physicalSource;
+    this.physicalSourceAlias = physicalSourceAlias;
+    this.dateSuffixEnabled = dateSuffixEnabled;
   }
 }
--- src/view.ts
+++ src/view.ts
@@ -1,7 +1,19 @@
 export abstract class View {
   type: string;
+  name: string;
+  alphabetName: string;
 
-  constructor({ type }: { type: string }) {
+  constructor({
+    type,
+    name,
+    alphabetName,
+  }: {
+    type: string;
+    name: string;
+    alphabetName: string;
+  }) {
     this.type = type;
+    this.name = name;
+    this.alphabetName = alphabetName;
   }
 }
