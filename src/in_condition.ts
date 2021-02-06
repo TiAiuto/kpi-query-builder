9a31a11e1fb7bfec43126685801c74f9be5df557
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-06 21:32:10 +0900

    :pencil2: in condition拡張

--- src/in_condition.ts
+++ src/in_condition.ts
@@ -1,7 +1,11 @@
 import { Condition } from "./condition";
+import { ValueSet } from "./value_set";
 
 export class InCondition extends Condition {
-  constructor() {
+  valueSet: ValueSet;
+
+  constructor({ valueSet }: { valueSet: ValueSet }) {
     super({ type: "in" });
+    this.valueSet = valueSet;
   }
 }
