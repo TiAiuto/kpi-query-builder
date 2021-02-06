0a6a83d8061d1f5231d3c2d734096d0fb0dfc4ac
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-06 23:51:03 +0900

    :heavy_plus_sign: query viewの定義修正

--- src/query_view.ts
+++ src/query_view.ts
@@ -1,7 +1,13 @@
 import { ReferenceView, ReferenceViewArgs } from "./reference_view";
 
 export class QueryView extends ReferenceView {
-  constructor(args: ReferenceViewArgs) {
+  columnsInheritanceEnabled: boolean;
+
+  constructor({
+    columnsInheritanceEnabled,
+    ...args
+  }: ReferenceViewArgs & { columnsInheritanceEnabled: boolean }) {
     super({ type: "query", ...args });
+    this.columnsInheritanceEnabled = columnsInheritanceEnabled;
   }
 }
