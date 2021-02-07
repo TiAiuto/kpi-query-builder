49c160cd14cc43f585340fb170374a2807b9326c
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-07 22:28:32 +0900

    :heavy_plus_sign: column検索実装

--- src/builder/phrase_resolution_context.ts
+++ src/builder/phrase_resolution_context.ts
@@ -20,4 +20,30 @@ export class PhraseResolutionContext {
     this.resolver = resolver;
     this.availableColumns = availableColumns;
   }
+
+  findColumnByName(sourceColumnName: string, source?: string): ResolvedColumn {
+    if (source) {
+      const completeMatchedColumn = this.availableColumns.find(
+        (column) =>
+          column.publicName === sourceColumnName &&
+          column.publicSource === source
+      );
+      if (completeMatchedColumn) {
+        return completeMatchedColumn;
+      }
+    }
+
+    const matchedColumns = this.availableColumns.filter(
+      (column) => column.publicName === sourceColumnName
+    );
+    if (matchedColumns.length === 1) {
+      return matchedColumns[0];
+    }
+    if (matchedColumns.length === 0) {
+      throw new Error(`${sourceColumnName}は未定義`);
+    }
+    throw new Error(
+      `${sourceColumnName}に該当するカラムが複数あるため特定できません`
+    );
+  }
 }
