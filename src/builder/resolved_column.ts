00d938cf1b745722f33b5ae369eed4e000dfa215
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2021-02-07 13:45:34 +0900

    :heavy_plus_sign: raw追加

--- src/builder/resolved_column.ts
+++ src/builder/resolved_column.ts
@@ -4,28 +4,36 @@ export class ResolvedColumn {
   publicSource?: string; // 外側に公開する定義名
   physicalSource?: string; // SQLの書き出し用
   physicalSourceColumnName?: string; // SQLの書き出し用
+  raw?: string; // 直にSQLを覚える場合
 
   constructor({
     publicName,
     physicalName,
     publicSource,
     physicalSource,
-    physicalSourceColumnName
+    physicalSourceColumnName, 
+    raw
   }: {
     publicName: string;
     physicalName: string;
     publicSource?: string;
     physicalSource?: string;
     physicalSourceColumnName?: string;
+    raw?: string;
   }) {
     this.publicName = publicName;
     this.physicalName = physicalName;
     this.publicSource = publicSource;
     this.physicalSource = physicalSource;
     this.physicalSourceColumnName = physicalSourceColumnName;
+    this.raw = raw;
   }
 
   toSQL(): string {
+    if (this.raw) {
+      return this.raw;
+    }
+
     if (!this.physicalSource) {
       throw new Error(`${this.publicName}の物理テーブル名が未定義`);
     }
