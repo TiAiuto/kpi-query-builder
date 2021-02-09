31d5228420337c2624035e0166ad3cc4030bea09
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-10 00:20:39 +0900

    :pencil2: transform valueの定義変更

--- src/builder/value/transform_value.ts
+++ src/builder/value/transform_value.ts
@@ -1,24 +1,20 @@
 import { SourceColumn } from "../source_column";
-import { ValueSurface } from "../value_surface";
 import { TransformPattern } from "../transform_pattern";
 import { Value } from "./value";
 
 export class TransformValue extends Value implements SourceColumn {
   source?: string;
   sourceColumnName: string;
-  output: ValueSurface;
   pattern: TransformPattern;
 
   constructor({
     source,
     sourceColumnName,
-    output,
     pattern,
-  }: SourceColumn & { output: ValueSurface; pattern: TransformPattern }) {
+  }: SourceColumn & { pattern: TransformPattern }) {
     super({ type: "transform" });
     this.source = source;
     this.sourceColumnName = sourceColumnName;
-    this.output = output;
     this.pattern = pattern;
   }
 
