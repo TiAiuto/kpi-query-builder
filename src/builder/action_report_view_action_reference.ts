4534ed5d36e6417aec50d3141b274ea7045c7318
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-21 22:20:35 +0900

    :pencil2: 条件を任意にした

--- src/builder/action_report_view_action_reference.ts
+++ src/builder/action_report_view_action_reference.ts
@@ -1,11 +1,17 @@
-import {Condition} from './condition/condition';
+import { Condition } from "./condition/condition";
 
 export class ActionReportViewActionReference {
-    actionName: string;
-    conditions: Condition[];
+  actionName: string;
+  conditions: Condition[];
 
-    constructor({actionName, conditions}: { actionName: string; conditions: Condition[]; }) {
-        this.actionName = actionName;
-        this.conditions = conditions;
-    }
+  constructor({
+    actionName,
+    conditions = [],
+  }: {
+    actionName: string;
+    conditions?: Condition[];
+  }) {
+    this.actionName = actionName;
+    this.conditions = conditions;
+  }
 }
