c516b63166e0d475b58f7846cbee6b5d67497984
ti-aiuto <ti.aiuto@outlook.jp>
2021-02-21 22:32:05 +0900

    :heavy_minus_sign: EqConditionの実装修正

--- src/builder/condition/eq_condition.ts
+++ src/builder/condition/eq_condition.ts
@@ -1,16 +1,7 @@
-import { ViewResolutionContext } from "../view_resolution_context";
 import { BinomialCondition, BinomialConditionArgs } from "./binomial_condition";
 
 export class EqCondition extends BinomialCondition {
   constructor(args: BinomialConditionArgs) {
     super({ ...args, template: "? = ?" });
-  }
-
-  toSQL(context: ViewResolutionContext): string {
-    return `${this.value.toSQL(context)} = ${this.otherValue.toSQL(context)}`;
-  }
-
-  toSQLForRoot(): string {
-    throw new Error("Method not implemented.");
   }
 }
