8e9aaf99bf3f987190cc56ea594009890fd8d366
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2021-02-25 16:28:14 +0900

    契約済みユーザの数値追加

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,172 @@ function main() {
     views: DefinedViews,
   });
 
+
+  const usersContractedUsageSummary = function () {
+    const timeColumnName = "タイムスタンプ";
+    const baseUnitName = "ユーザコード";
+
+    const periodUnitType = "タイムスタンプ_月抽出";
+    const periodUnitName = "基準アクション月";
+    const periodUnitAlphabetName = "base_action_month";
+
+    const baseActionName = "A_PLUS利用開始";
+
+    const relatedActionNames = [
+      "A_概観_サイト内の任意ページ表示",
+      "A_概観_勉強会内の任意ページ表示",
+      "A_概観_勉強会過去動画内の任意ページ表示",
+      "A_概観_個別ケース相談内の任意ページ表示",
+      "A_概観_教材内の任意ページ表示",
+      "A_概観_ヒント動画内の任意ページ表示",
+      "A_概観_マイページ内の任意ページ表示",
+      "A_TOP表示",
+      "A_マイページTOP表示",
+      "A_ケース相談TOP表示",
+      "A_ケース相談詳細表示",
+      "A_ケース相談一次相談作成",
+      "A_ケース相談一次相談申込",
+      "A_ケース相談申込詳細表示",
+      "A_ケース相談二次相談作成",
+      "A_ケース相談二次相談提出",
+      "A_勉強会TOP表示",
+      "A_勉強会詳細表示",
+      "A_勉強会申込",
+      "A_勉強会申込詳細表示",
+      "A_勉強会参加",
+    ];
+
+    const generateAggregateColumns = function (
+      view: ResolvedView,
+      withSourceParam: boolean,
+      index: number
+    ): ValueSurface[] {
+      const result = [
+        new ValueSurface({
+          name: periodUnitName,
+          alphabetName: periodUnitAlphabetName,
+          value: new TransformValue({
+            sourceColumnName: timeColumnName,
+            source: view.publicName,
+            pattern: new TransformPattern({ name: periodUnitType }),
+          }),
+        }),
+        new ValueSurface({
+          name: `アクション集計値`,
+          alphabetName: `action_aggregated_value`,
+          value: new AggregateValue({
+            pattern: new AggregatePattern({
+              name: "COUNT_DISTINCT",
+            }),
+            source: view.publicName,
+            sourceColumnName: baseUnitName,
+          }),
+        }),
+        new ValueSurface({
+          name: "アクション種別ラベル",
+          alphabetName: "action_type_label",
+          value: new RawValue({ raw: `'${index}_${view.publicName}'` }),
+        }),
+      ];
+
+      return result;
+    };
+
+    const generateGroupBy = function (
+      view: ResolvedView,
+      withSourceParam: boolean
+    ): Group[] {
+      const result = [
+        new Group({
+          value: new TransformValue({
+            sourceColumnName: timeColumnName,
+            source: view.publicName,
+            pattern: new TransformPattern({ name: periodUnitType }),
+          }),
+        }),
+      ];
+
+      return result;
+    };
+
+    resolver.addView(
+      new RoutineView({
+        name: "集計期間基準集合クエリ",
+        alphabetName: "aggregate_base_period",
+        pattern: new RoutinePattern({
+          name: "期間集合生成",
+          args: ["月単位", "20201001", "20211231"],
+        }),
+      })
+    );
+
+    const reportUnionView = new UnionView({
+      name: "集計クエリ",
+      alphabetName: "aggregated_view",
+      views: [
+        new QueryView({
+          name: "契約者分母",
+          alphabetName: "contracted_users",
+          source: "集計期間基準集合クエリ",
+          joins: [
+            new InnerJoin({
+              target: "ユーザコード付きPLUS契約",
+              conditions: [
+                new RawCondition({
+                  raw:
+                    'DATE(usage_start_date_timestamp, "Asia/Tokyo") <= date_range_end AND ' +
+                    '(usage_end_date_timestamp IS NULL OR date_range_end <= DATE(usage_end_date_timestamp, "Asia/Tokyo"))',
+                }),
+              ],
+            }),
+          ],
+          inheritColumns: ["期間生成値"],
+          columns: [
+            new ValueSurface({
+              name: "契約ユーザ数",
+              alphabetName: "action_aggregated_value",
+              value: new AggregateValue({
+                sourceColumnName: "契約ユーザコード",
+                pattern: new AggregatePattern({ name: "COUNT" }),
+              }),
+            }),
+
+            new ValueSurface({
+              name: "アクション種別ラベル",
+              alphabetName: "action_type_label",
+              value: new RawValue({ raw: `'0_期間内PLUS契約中'` }),
+            }),
+          ],
+          groups: [
+            new Group({
+              value: new SelectValue({ sourceColumnName: "期間生成値" }),
+            }),
+          ],
+        }),
+        ...relatedActionNames.map((relatedActionName, index) => {
+          const relatedActionView = resolver.resolve(relatedActionName);
+          return new QueryView({
+            name: `内側関連アクション集計用`,
+            alphabetName: `inner_related_for_aggregation`,
+            source: relatedActionView.publicName,
+            columns: generateAggregateColumns(
+              relatedActionView,
+              true,
+              index + 1
+            ),
+            groups: generateGroupBy(relatedActionView, true),
+            conditions: [
+              new RawCondition({
+                raw: 'DATE(time, "Asia/Tokyo") >= DATE("2020-10-01")',
+              }),
+            ],
+          });
+        }),
+      ],
+    });
+    resolver.addView(reportUnionView);
+  };
+
   const usersContractedSourceParam = function () {
     const timeColumnName = "タイムスタンプ";
     const baseUnitName = "ユーザコード";
@@ -391,7 +557,8 @@ function main() {
   };
 
   // usersAfterContract();
-  usersContractedSourceParam();
+  // usersContractedSourceParam();
+  usersContractedUsageSummary();
 
   const bootstrapViewName = "集計クエリ";
 
