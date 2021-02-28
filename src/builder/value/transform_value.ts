7582df75c8629a529c913c2f4c0fb7538ac03577
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2021-02-28 14:00:12 +0900

    定義変更

--- src/builder/value/transform_value.ts
+++ src/builder/value/transform_value.ts
@@ -1,37 +1,30 @@
 import { ViewResolutionContext } from "../view_resolution_context";
-import { SourceColumn } from "../source_column";
 import { TransformPattern } from "../transform_pattern";
 import { Value } from "./value";
 
-export class TransformValue extends Value implements SourceColumn {
-  source?: string;
-  sourceColumnName: string;
+export class TransformValue extends Value {
+  value: Value;
   pattern: TransformPattern;
 
   constructor({
-    source,
-    sourceColumnName,
+    value,
     pattern,
-  }: SourceColumn & { pattern: TransformPattern }) {
+  }: { value: Value; pattern: TransformPattern }) {
     super({ type: "transform" });
-    this.source = source;
-    this.sourceColumnName = sourceColumnName;
+    this.value = value;
     this.pattern = pattern;
   }
 
   toSQL(context: ViewResolutionContext): string {
-    const resolvedColumn = context.findColumnByName(
-      this.sourceColumnName,
-      this.source
-    );
+    const valueSql = this.value.toSQL(context);
     if (this.pattern.name === "タイムスタンプ_月抽出") {
-      return `FORMAT_TIMESTAMP('%Y-%m', ${resolvedColumn.fullPhysicalName}, 'Asia/Tokyo')`;
+      return `FORMAT_TIMESTAMP('%Y-%m', ${valueSql}, 'Asia/Tokyo')`;
     } else if (this.pattern.name === "タイムスタンプ_週抽出") {
-      return `CONCAT(FORMAT_TIMESTAMP('%Y-%m-%d', TIMESTAMP_TRUNC(${resolvedColumn.fullPhysicalName}, WEEK(MONDAY), 'Asia/Tokyo'), 'Asia/Tokyo'), ', ', FORMAT_TIMESTAMP('%Y-%m-%d', TIMESTAMP_ADD(TIMESTAMP_TRUNC(${resolvedColumn.fullPhysicalName}, WEEK(MONDAY), 'Asia/Tokyo'), INTERVAL 6 DAY), 'Asia/Tokyo'))`;
+      return `CONCAT(FORMAT_TIMESTAMP('%Y-%m-%d', TIMESTAMP_TRUNC(${valueSql}, WEEK(MONDAY), 'Asia/Tokyo'), 'Asia/Tokyo'), ', ', FORMAT_TIMESTAMP('%Y-%m-%d', TIMESTAMP_ADD(TIMESTAMP_TRUNC(${valueSql}, WEEK(MONDAY), 'Asia/Tokyo'), INTERVAL 6 DAY), 'Asia/Tokyo'))`;
     } else if (this.pattern.name === "タイムスタンプ_日抽出") {
-      return `FORMAT_TIMESTAMP('%Y-%m-%d', ${resolvedColumn.fullPhysicalName}, 'Asia/Tokyo')`;
+      return `FORMAT_TIMESTAMP('%Y-%m-%d', ${valueSql}, 'Asia/Tokyo')`;
     } else if (this.pattern.name === "空白変換") {
-      return `IF(${resolvedColumn.fullPhysicalName} IS NULL OR ${resolvedColumn.fullPhysicalName} = '', '${this.pattern.args[0]}', ${resolvedColumn.fullPhysicalName})`; // TODO: 本当はこういうところエスケープしないといけない
+      return `IF(${valueSql} IS NULL OR ${valueSql} = '', '${this.pattern.args[0]}', ${valueSql})`; // TODO: 本当はこういうところエスケープしないといけない
     } else {
       throw new Error(`${this.pattern.name}は未実装`);
     }
