a9235fa0d616fa1eb570d9071cb9410d04c6f057
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2021-03-04 14:18:07 +0900

    月別過去動画視聴回数も追加

--- src/main.ts
+++ src/main.ts
@@ -1633,6 +1633,67 @@ function main() {
     resolver.addView(reportUnionView);
   };
 
+  const userHealthScoreStatisticsMonthStudyMeetingArchiveVideoPlay = function () {
+    const reportUnionView = new UnionView({
+      name: "集計クエリ",
+      alphabetName: "aggregated_view",
+      views: [
+        new QueryView({
+          name: "",
+          alphabetName: "",
+          source: "A_勉強会過去動画再生開始",
+          columns: [
+            new ValueSurface({
+              name: "統計値",
+              alphabetName: "stat_value",
+              value: new TransformValue({
+                pattern: new TransformPattern({ name: "型変換_文字列" }),
+                value: new AggregateValue({
+                  pattern: new AggregatePattern({
+                    name: "COUNT",
+                  }),
+                  value: new SelectValue({
+                    sourceColumnName: "タイムスタンプ",
+                  }),
+                }),
+              }),
+            }),
+            new ValueSurface({
+              name: "集計期間",
+              alphabetName: "period",
+              value: new TransformValue({
+                pattern: new TransformPattern({ name: "タイムスタンプ_月抽出" }),
+                value: new SelectValue({
+                  sourceColumnName: "タイムスタンプ",
+                }),
+              }),
+            }),
+            new ValueSurface({
+              name: "統計種別ラベル",
+              alphabetName: "stat_label",
+              value: new ConstStringValue({ value: "PLUS全体_月別_勉強会_過去動画視聴回数" }),
+            }),
+          ],
+          inheritColumns: ["ユーザコード"],
+          groups: [
+            new Group({
+              value: new SelectValue({ sourceColumnName: "ユーザコード" }),
+            }),
+            new Group({
+              value: new TransformValue({
+                pattern: new TransformPattern({ name: "タイムスタンプ_月抽出" }),
+                value: new SelectValue({
+                  sourceColumnName: "タイムスタンプ",
+                }),
+              }),
+            }),
+          ],
+        }),
+      ],
+    });
+    resolver.addView(reportUnionView);
+  };
+
 
   // usersAfterContract();
   // usersContractedUsageSummary();
@@ -1643,7 +1704,8 @@ function main() {
   // userHealthScoreStatisticsMonthPv();
   // userHealthScoreStatisticsMonthKyozaiLessonPv();
   // userHealthScoreStatisticsMonthKyozaiPdfClick();
-  userHealthScoreStatisticsMonthHintVideoPlay();
+  // userHealthScoreStatisticsMonthHintVideoPlay();
+  // userHealthScoreStatisticsMonthStudyMeetingArchiveVideoPlay();
 
   const bootstrapViewName = "集計クエリ";
 
